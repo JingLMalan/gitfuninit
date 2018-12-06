c439e3da0235c3d8d180ff773d4adbf46d7473e8
jing.l.mlana <jing.l.malan@gmail.com>
2018-12-06 09:54:36 +0800

    add logic of github && create repository

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+#!/usr/bin/env node
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const files = require('./lib/files');
 const inquirer  = require('./lib/inquirer');
 const Configstore = require('configstore');
-const CLI = require('clui');
-const Spinner = CLI.Spinner;
-
+const github = require('./lib/github');
 clear();
 
 console.log(
@@ -17,19 +16,65 @@ console.log(
 
 console.log(`\n正在加载....\n`);
 
+const run = async () => {
+  // let token = github.getStoredGithubToken();
+  // if(!token) {
+  //   await github.setGithubCredentials();
+  //   token = await github.registerNewToken();    
+  // }
+  // console.log(token);
+   try {
+    // Retrieve & Set Authentication Token
+    const token = await getGithubToken();
+    github.githubAuth(token);
+
+    // Create remote repository
+    const url = await repo.createRemoteRepo();
+
+    // Create .gitignore file
+    await repo.createGitignore();
+
+    // Set up local repository and push to remote
+    const done = await repo.setupRepo(url);
+    if(done) {
+      console.log(chalk.green('All done!'));
+    }
+  } catch(err) {
+      if (err) {
+        switch (err.code) {
+          case 401:
+            console.log(chalk.red('Couldn\'t log you in. Please provide correct credentials/token.'));
+            break;
+          case 422:
+            console.log(chalk.red('There already exists a remote repository with the same name'));
+            break;
+          default:
+            console.log(err);
+        }
+      }
+  }
+}
+
+const getGithubToken = async () => {
+  // Fetch token from config store
+  let token = github.getStoredGithubToken();
+  if(token) {
+    return token;
+  }
+
+  // No token found, use credentials to access GitHub account
+  await github.setGithubCredentials();
+
+  // register new token
+  token = await github.registerNewToken();
+  return token;
+}
+run();
 
 // if (files.directoryExists('.git')) {
 //   console.log(chalk.red('git仓库已经存在了，创建失败，请检查!'));
 //   process.exit();
 // }
 
-const run = async () => {
-  const credentials = await inquirer.askGithubCredentials();
-  const status = new Spinner('认证正在进行中，请稍后...');
-  status.start();
-  
-}
-
-run();
 
 // const conf = new Configstore('ginit');
